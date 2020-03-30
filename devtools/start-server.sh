3f39087c6ae3d94f3698b7d28b2609edc34f42b6
Roman Buchholz <roman.buchholz@t-online.de>
2020-03-30 18:04:19 +0200

    added mount to copy files into docker container before server start

--- devtools/start-server.sh
+++ devtools/start-server.sh
@@ -4,5 +4,5 @@ if [[ $(sudo docker ps -a | grep spigot-server) ]]
 then
   sudo docker start -ia spigot-server
 else
-  sudo docker run -it -p 5005:5005 -p 25565:25565 --name spigot-server --user 1000:1000 -v "$(pwd)/plugins":/spigot/plugins spigot-server
+  sudo docker run -it -p 5005:5005 -p 25565:25565 --name spigot-server --user 1000:1000 -v "$(pwd)/plugins":/spigot/plugins -v "$(pwd)/server-config":/spigot-config spigot-server
 fi
--- devtools/start.sh
+++ devtools/start.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+cp -a /spigot-config/. .
+
 java -Xms1G -Xmx1G -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -jar spigot.jar
